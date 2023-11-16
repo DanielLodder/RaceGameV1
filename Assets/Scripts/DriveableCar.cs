d95522283ba078aeef6a0cd26838e0b6697e9aa3
DanielLodder <94366248+DanielLodder@users.noreply.github.com>
2023-11-16 01:06:40 +0100

    PlayerMovement WIP
    there are a few things wrong with it. But I havent narrowed it down with what's wrong with it.
    Velocity seems broken somehow where it sometimes randomly speedsup to insane amounts and leaves the boundries of unity

--- Assets/Scripts/DriveableCar.cs
+++ Assets/Scripts/DriveableCar.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DriveableCar : MonoBehaviour
 {
     public Rigidbody rigidBody;
+    private PlayerInput playerInput;
 
     [SerializeField] private float maxSpeed;
     [SerializeField] private float breakForce;
@@ -13,13 +15,48 @@ public class DriveableCar : MonoBehaviour
     [SerializeField] private float turnAngle;
     [Range(0, 1)]
     [SerializeField] private float traction;
-    void Start()
-    {
 
+    PlayerInputSystem playerInputSystem;
+    public InputActionAsset actions;
+    private InputAction moveAction;
+    private InputAction turnAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = actions.FindActionMap("Player").FindAction("Driving");
+        turnAction = actions.FindActionMap("Player").FindAction("Stearing");
+
+
+        PlayerInputSystem inputActions = new PlayerInputSystem();
+        inputActions.Player.Enable();
     }
-
-    void Update()
+    private void Update()
     {
+        Drive();
+        Stearing();
+    }
+    private void Drive()
+    {
+        Vector3 rawMove = moveAction.ReadValue<Vector3>() * (accelerationSpeed * 10);
 
+        Vector3 movementVector = transform.position * rigidBody.position.x + transform.forward * rawMove.z * Time.deltaTime;
+        movementVector.y = rigidBody.position.y;
+        rigidBody.velocity = movementVector;
+    }
+    private void Stearing()
+    {
+        Vector3 rawTurn = turnAction.ReadValue<Vector3>() * turnAngle;
+        Vector3 rotation = Vector3.up * rawTurn.x;
+        Debug.Log(rawTurn);
+        transform.localEulerAngles += rotation * Time.deltaTime;
+    }
+    private void OnEnable()
+    {
+        actions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        actions.FindActionMap("Player").Disable();
     }
 }
