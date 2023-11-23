2c15ff0a1bae870db75b4ed09aedea3d4a558777
DanielLodder <94366248+DanielLodder@users.noreply.github.com>
2023-11-23 11:48:09 +0100

    driving update

--- Assets/Scripts/DriveableCar.cs
+++ Assets/Scripts/DriveableCar.cs
@@ -9,20 +9,25 @@ public class DriveableCar : MonoBehaviour
     private PlayerInput playerInput;
 
     [SerializeField] private float maxSpeed;
-    [SerializeField] private float breakForce;
-    [SerializeField] private float decelerationSpeed;
-    [SerializeField] private float accelerationSpeed;
     [SerializeField] private float turnAngle;
     [Range(0, 1)]
     [SerializeField] private float traction;
+
 
     PlayerInputSystem playerInputSystem;
     public InputActionAsset actions;
     private InputAction moveAction;
     private InputAction turnAction;
 
+<<<<<<< Updated upstream
+=======
+    [Range(1,6)]
+    [SerializeField] private int gear;
+
+>>>>>>> Stashed changes
     private void Awake()
     {
+        gear = 1;
         playerInput = GetComponent<PlayerInput>();
         moveAction = actions.FindActionMap("Player").FindAction("Driving");
         turnAction = actions.FindActionMap("Player").FindAction("Stearing");
@@ -31,41 +36,92 @@ public class DriveableCar : MonoBehaviour
         PlayerInputSystem inputActions = new PlayerInputSystem();
         inputActions.Player.Enable();
     }
+
     private void Update()
     {
         Drive();
     }
+
     public void Drive()
     {
+        //adds velocity to the vehicle and allows it to turn.
         Vector3 localVelocity = transform.InverseTransformDirection(rigidBody.velocity);
-        Vector2 velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.z);
 
-        velocity = velocity.normalized * maxSpeed;
         localVelocity = localVelocity.normalized * maxSpeed;
 
-        float speedPercentage = velocity.magnitude / maxSpeed;
         Vector3 rawTurn = turnAction.ReadValue<Vector3>();
-        Vector3 rotation = Vector3.up * turnAngle * rawTurn.x * speedPercentage * Time.deltaTime;
+        Vector3 rotation = Vector3.up * turnAngle * rawTurn.x * Time.deltaTime;
 
-        if (localVelocity.z > 0)
+        if (localVelocity.z >= 0)
         {
             transform.localEulerAngles += rotation;
         }
-        else if (localVelocity.z < 0)
+        else if (localVelocity.z <= 0)
         {
             transform.localEulerAngles -= rotation;
         }
-        Vector3 rawMove = moveAction.ReadValue<Vector3>() * accelerationSpeed;
+
+        Vector3 rawMove = moveAction.ReadValue<Vector3>() * maxSpeed;
         rigidBody.AddForce(transform.rotation * Vector3.forward * rawMove.z);
-        Debug.Log(rawMove);
-        Debug.Log(rawTurn);
     }
+<<<<<<< Updated upstream
+=======
+
+    private void Gears()
+    {
+        //sets the gear ratio of vehicle which changes your max speed.
+        switch (gear)
+        {
+            case 1:
+                maxSpeed = 2;
+                break;
+            case 2:
+                maxSpeed = 2.5f;
+                break;
+            case 3:
+                maxSpeed = 3;
+                break;
+            case 4:
+                maxSpeed = 3.5f;
+                break;
+            case 5:
+                maxSpeed = 4;
+                break;
+            case 6:
+                maxSpeed = 4.5f;
+                break;
+        }
+        if (gear == 0)
+        {
+            gear = 1;
+        }
+        if (gear == 7)
+        {
+            gear = 6;
+        }
+    }
+
+    //the inputs for the gear shifts.
+    private void ShiftUp(InputAction.CallbackContext context)
+    {
+        gear++;
+        Debug.Log("shifted Up");
+    }
+    private void ShiftDown(InputAction.CallbackContext context)
+    {
+        gear--;
+        Debug.Log("shifted Down");
+    }
+
+>>>>>>> Stashed changes
     private void OnEnable()
     {
+        //enables all the actions so the system can detect when you pressed the button.
         actions.FindActionMap("Player").Enable();
     }
     private void OnDisable()
     {
+        //disables all the actions so the system can detect when you stopped pressing the button.
         actions.FindActionMap("Player").Disable();
     }
 }
